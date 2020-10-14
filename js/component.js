258dabd34983f684e47155cdbcf6115ab4547ee3
Rajesh Mondal <rajeshmondal9007@gmail.com>
2020-10-14 23:07:31 +0530

    Initial Commit

---
+++ js/component.js
@@ -0,0 +1,167 @@
+function WelcomeScreen() {
+  return(
+    <div className="welcome-screen">
+      <img src="images/preview.jpg" alt="" />
+    </div>
+  )
+}
+
+function InitScreen() {
+  
+  const [dname, setDname] = React.useState(null)
+  
+  const SignIn = ()=>{
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(function() {
+        firebase.auth().signInAnonymously().then(()=>{
+          let user = firebase.auth().currentUser
+          firebase.database().ref("Users/" +user.uid).set({
+            name: dname,
+            uid: user.uid
+          })
+        }).catch(function(error) {
+          console.log(error.code, error.message);
+        });
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+  }
+  
+  const getName = (name)=>{
+    setDname(name)
+  }
+  
+  
+  return(
+    <div className="init-screen">
+      <img src="images/avatar.png" alt="" />
+      <p className="lead">Create Account</p>
+      <div className="form-group">
+        <input type="text" placeholder="Your Name" className="form-control" id="displayName" onKeyUp={(event)=>getName(event.target.value)} />
+      </div>
+      
+      <div>
+        <button className="btn btn-warning btn-block" onClick={()=>SignIn()}>Create</button>
+      </div>
+    </div>
+  )
+}
+
+function Home() {
+  const [recipient, setRecipient] = React.useState(null)
+  
+  React.useEffect(()=>{
+    var user = firebase.auth().currentUser;
+    firebase.database().ref("Users").on('value', (snapshot)=>{
+      let Users = []
+      snapshot.forEach((child)=>{
+        if(child.val().uid != user.uid) {
+          Users.unshift(child.val())
+        }
+      })
+      setRecipient(Users)
+    })
+    // Set Last Seen
+    setInterval(()=>{
+      firebase.database().ref("Users/"+user.uid+"/lastSeen").set(new Date().getTime())
+    }, 1000)
+  }, [])
+  
+  
+  return(
+    <div>
+      {recipient?recipient.map((item)=>
+        <div className="card" key={item.uid} onClick={()=>{
+          store.dispatch({ type: 'CHATROOM' })
+          store.dispatch({ 
+            type: 'SET', 
+            data: item
+          })
+        }}>
+          <img src="images/avatar.png" alt="avatar" />
+          <div>
+            <p className="lead">{item.name}</p>
+            <p className="text-danger">Last Seen: {new Date(item.lastSeen).getHours()+":"+new Date(item.lastSeen).getMinutes()}</p>
+          </div>
+        </div>
+      ):null}
+    </div>
+  )
+}
+
+
+function ChatRoom() {
+  
+  const [receiver, setReceiver] = React.useState(store.getState().Recipient.uid)
+  
+  const [sender, setSender] = React.useState(firebase.auth().currentUser.uid)
+  
+  const [channel, setChannel] = React.useState(()=>{
+    if (sender < receiver) {
+      return sender + receiver
+    } else {
+      return receiver + sender
+    }
+  })
+  
+  const [message, setMessage] = React.useState(null)
+  
+  React.useEffect(()=>{
+    firebase.database().ref(channel).on('value', (snapshot)=>{
+      let msg = []
+      snapshot.forEach((child)=>{
+        msg.push(child.val())
+      })
+      setMessage(msg)
+    })
+    document.getElementById("text-send").addEventListener('keyup', (event)=>{
+      if(event.key == 'Enter') {
+        Send()
+      }
+    })
+  }, [])
+  
+  const Send = ()=>{
+    let txt = document.getElementById("text-send").value
+    firebase.database().ref(channel).push({
+      senderId: sender, 
+      receiveId: receiver, 
+      timeStamp: new Date().getTime(),
+      message: txt
+    })
+    document.getElementById("text-send").value = ""
+  }
+  
+  return(
+   <div className="chat-room">
+   {message?message.map((item)=>
+     <>
+      {item.senderId==sender?<SendMsg msg={item.message} />:<ReceiveMsg msg={item.message} />}
+     </>
+   ):'Say Hi'}
+    <div className="form-group bottom">
+      <input type="text" placeholder="Type Your Messages" className="form-control" id="text-send" />
+      <label onClick={()=>Send()}><ion-icon name="send-sharp"></ion-icon></label>
+    </div>
+   </div>
+  )
+}
+
+function ReceiveMsg({ msg }) {
+  return(
+    <div className="left">
+      <div className="card">{msg}</div>
+    </div>
+  )
+}
+
+function SendMsg({ msg }) {
+  return(
+    <div className="right">
+      <div className="card">{msg}</div>
+    </div>
+  )
+}
---
+++ js/main.js
@@ -0,0 +1,87 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCl5YJ26-YKii_c7HK31kuZQRvr2y-IoWQ",
+  authDomain: "chatrobot-2000.firebaseapp.com",
+  databaseURL: "https://chatrobot-2000.firebaseio.com",
+  projectId: "chatrobot-2000",
+  storageBucket: "chatrobot-2000.appspot.com",
+  messagingSenderId: "830846027124",
+  appId: "1:830846027124:web:0e975b81e5defeb5"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const Auth = firebase.auth()
+
+function ScreenReducer(screen=0,action) {
+  switch(action.type) {
+    case 'WELCOME':
+      screen=0
+      return screen
+      
+    case 'INIT':
+      screen=1
+      return screen
+      
+    case 'HOME':
+      screen=2
+      return screen
+      
+    case 'CHATROOM':
+      screen=3
+      return screen
+      
+    default:
+      return screen
+  }
+}
+
+function RecipientReducer(recipient=null, action) {
+  switch(action.type) {
+    case 'SET':
+      recipient=action.data
+      return recipient
+      
+    default:
+      return recipient
+  }
+}
+
+let Reducer = Redux.combineReducers({
+  Screen: ScreenReducer, 
+  Recipient: RecipientReducer
+})
+
+let store = Redux.createStore(Reducer)
+
+
+function App() {
+  
+  const [screen, setScreen] = React.useState(store.getState().Screen)
+  
+  React.useEffect(()=>{
+    // Check Auth
+    setTimeout(()=>{
+    Auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user.uid)
+        store.dispatch({ type: 'HOME' })
+      } else {
+        store.dispatch({ type: 'INIT' })
+      }
+    })
+    }, 2000)
+    // Set Screen on subscribe
+    store.subscribe(()=>{
+      setScreen(store.getState().Screen)
+    })
+  }, [])
+  
+  return(
+    <>
+      {screen==0?<WelcomeScreen />:screen==1?<InitScreen />:screen==2?<Home />:screen==3?<ChatRoom />:null}
+    </>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
